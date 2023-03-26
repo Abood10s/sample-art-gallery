116fc03071e69027b2708b9f8b09647b55eb7e55
Abood10s <saafeenabood@gmail.com>
2023-03-26 10:52:19 +0300

    removing conflicting package react-sound from the deployment

--- src/components/GalleryItem/style.js
+++ src/components/GalleryItem/style.js
@@ -86,6 +86,9 @@ export const Grid = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+  }
 `;
 export const Music = styled.button`
   display: flex;
